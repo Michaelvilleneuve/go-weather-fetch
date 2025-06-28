c16600a9fad60f2cc46f34cce895450bde13953a
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2025-06-28 23:20:57 +0200

    Serving public folder

--- internal/server/server.go
+++ internal/server/server.go
@@ -10,10 +10,15 @@ import (
 
 func Serve() {
 	utils.LoadEnv()
-	forecast.Serve()
+	if os.Getenv("WORKER") != "false" || os.Getenv("SERVER") == "true" {
+		forecast.Serve()
+	}
+	
 	http.HandleFunc("/up", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("ha ha ha ha staying alive"))
 	})
+
+	http.Handle("/", http.FileServer(http.Dir("public")))
 
 	utils.Log("Serving forecast on port " + os.Getenv("PORT"))
 	http.ListenAndServe(":" + os.Getenv("PORT"), nil)
